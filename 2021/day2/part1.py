dae42135a8682bd276aee100d7e237d55259e327
dchae <daniel.jiwoong@gmail.com>
2021-12-01 21:09:47 -0800

    day 2 commit

--- 2021/day2/part1.py
+++ 2021/day2/part1.py
@@ -1,5 +1,5 @@
 inputdata = []
-with open(r"G:\My Drive\Work\Coding\AdventofCode\2021\day2\input1.txt") as inputfile:
+with open(r"C:\github-dchae\Advent_of_Code\2021\day2\input1.txt") as inputfile:
     for line in inputfile:
         inputdata.append(int(line.strip()))
 print(inputdata)
