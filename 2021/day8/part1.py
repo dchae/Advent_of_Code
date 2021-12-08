69aa65e6d77482a709e0ed00e6755e678bbcd461
dchae <daniel.jiwoong@gmail.com>
2021-12-07 22:47:22 -0800

    partial save

--- 2021/day8/part1.py
+++ 2021/day8/part1.py
@@ -1,17 +1,39 @@
-istestcase = True
+istestcase = False
 
 inputfilename = ""  # Testcase switcher
 if istestcase:
-    inputfilename = r"C:\github-dchae\Advent_of_Code\2021\day7\exinput1.txt"
+    inputfilename = r"C:\github-dchae\Advent_of_Code\2021\day8\exinput1.txt"
 
 else:
-    inputfilename = r"C:\github-dchae\Advent_of_Code\2021\day7\input1.txt"
+    inputfilename = r"C:\github-dchae\Advent_of_Code\2021\day8\input1.txt"
 
-inputdata = []
-with open(inputfilename) as inputfile:  # read input and initialise inputdata
+signalpatterns = []
+outputvalues = []
+with open(
+    inputfilename
+) as inputfile:  # read input and initialise signalpatterns, outputvalues
     lines = inputfile.readlines()
     for line in lines:
-        line = line.strip().split(",")
-        for number in line:
-            inputdata.append(int(number))
-print(inputdata)
+        line = line.strip().split(" | ")
+        signalpatterns.append(line[0].split(" "))
+        outputvalues.append(line[1].split(" "))
+print(signalpatterns)
+print(outputvalues)
+
+# 1 needs 2 digits, 4:4, 7:3, 8:7
+def count1478(outputvalues):
+    count = 0
+    for row in outputvalues:
+        for out_val in row:
+            if len(out_val) == 2:  # if 1
+                count += 1
+            if len(out_val) == 4:  # if 4
+                count += 1
+            if len(out_val) == 3:  # if 7
+                count += 1
+            if len(out_val) == 7:  # if 8
+                count += 1
+    return count
+
+
+print(count1478(outputvalues))
