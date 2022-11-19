cf2df0c89ad356d967d927fb2ac55953ed4552ed
dchae <daniel.j.chae@gmail.com>
2022-11-19 14:26:59 -0800

    day15 started

---
+++ 2021/day15/day15-1.py
@@ -0,0 +1,14 @@
+"""
+input: string
+output: int
+
+- find the path with lowest total risk
+- start at top left, end at bottom right
+- risk is incurred when you *enter* a position
+    - starting square incurs no risk
+
+ds: dict, queue
+
+algo: Dijkstra
+
+"""
