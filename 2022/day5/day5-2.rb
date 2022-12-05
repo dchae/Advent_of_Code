ba9c4e48eb1ad6ba6d14271830c46e9cd8795880
dchae <daniel.j.chae@gmail.com>
2022-12-04 21:40:38 -0800

    Day 5

---
+++ 2022/day5/day5-2.rb
@@ -0,0 +1,24 @@
+lines = File.read("2022/day5/input.txt").split("\n")
+
+crates = Hash.new { |h, k| h[k] = [] }
+
+line_idx = 0
+while true
+  line = lines[line_idx]
+  break if line.empty?
+  line.chars.each_with_index { |c, i| crates[i] << c if c =~ /[A-Z]/ }
+  line_idx += 1
+end
+
+instructions = []
+(line_idx + 1...lines.size).each do |i|
+  instructions << lines[i].gsub(/[^\d]/, " ").split.map(&:to_i)
+end
+
+crates = [[]] + crates.keys.sort.map { |k| crates[k] }
+
+instructions.each do |quantity, from, to|
+  crates[to].unshift(*crates[from].slice!(0, quantity))
+end
+
+p crates.map { |arr| arr.first }.join("")
