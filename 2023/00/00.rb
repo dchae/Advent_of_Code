d2b53fdcbd3a02557ea58e9ee0dcb53d2e1ea0b5
dchae <daniel.j.chae@gmail.com>
2023-12-03 23:50:41 -0800

    day4

--- 2023/00/00.rb
+++ 2023/00/00.rb
@@ -2,6 +2,11 @@ def filepath(filename, subfolder = '')
   File.join(*[File.expand_path('..', __FILE__), subfolder, filename].compact)
 end
 
-# path = filepath('input.txt')
+path = filepath('input.txt')
 path = filepath('exinput.txt')
 input = File.read(path)
+
+# Part 1
+
+
+# Part 2
---
+++ generate_day.rb
@@ -0,0 +1,45 @@
+require 'fileutils'
+require 'date'
+
+def filepath(filename, subfolder = '')
+  File.join(*[File.expand_path('..', __FILE__), subfolder, filename].compact)
+end
+
+def get_cur_yr_day
+  parent_dir = Date.today.year.to_s
+  [parent_dir, Dir.children(filepath(nil, parent_dir)).max.succ]
+end
+
+def cur_day_dirpath(filename = nil)
+  parent_dir, cur_day = get_cur_yr_day
+  dir = File.join(parent_dir, cur_day)
+  filepath(filename, dir)
+end
+
+def create_file(filepath, file_content = '')
+  dirname = File.dirname(filepath)
+  FileUtils.mkdir_p(dirname) unless File.directory?(dirname)
+
+  File.open(filepath, 'w') { |f| f.write(file_content) }
+end
+
+template = <<TEMPLATE
+def filepath(filename, subfolder = '')
+  File.join(*[File.expand_path('..', __FILE__), subfolder, filename].compact)
+end
+
+path = filepath('input.txt')
+path = filepath('exinput.txt')
+input = File.read(path)
+
+# Part 1
+
+
+# Part 2
+TEMPLATE
+cur_year, cur_day = get_cur_yr_day
+subfolder = File.join(get_cur_yr_day)
+
+create_file(filepath(cur_day + ".rb", subfolder), template)
+create_file(filepath("exinput.txt", subfolder))
+create_file(filepath("input.txt", subfolder))
